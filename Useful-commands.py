dc4fd704fa91814cf05552a1f3b5078e76f0f827
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-30 11:07:34 +0800

    Add files via upload

---
+++ Useful-commands.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+$ python example_name.py
+
+Users⁩/⁨pauline⁩⁩/⁨Documents⁩⁩/⁨Python⁩⁩/⁨seaborn-data-master⁩
+
+$ python3.7 -m pip install scikit-learn
+
+plt.show()
+
+jupyter notebook
+
+
+import os
+os.getcwd() # текущая директория
+os.chdir('/Users/pauline/Documents/Python')
+os.getcwd()
+
+exec(open('fibo.py').read()) # читать скрипт в IDLE
+
+The IDLE shell window is not the same as a terminal shell (e.g. running sh or bash). Rather, it is just like being in the Python interactive interpreter (python -i). The easiest way to run a script in IDLE is to use the Open command from the File menu (this may vary a bit depending on which platform you are running) to load your script file into an IDLE editor window and then use the Run -> Run Module command (shortcut F5). 
